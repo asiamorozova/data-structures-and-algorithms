0acf33448c6bf0bb6bea3ce245e95bb4648136b0
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-09 17:44:56 -0700

    code challege passed

---
+++ challenges/arrayMap/array-map.js
@@ -0,0 +1,18 @@
+
+const map = (arr, func) => {
+    const result = []
+    for(let i = 0; i < arr.length; i++){
+        result[i] = func(arr[i])
+    }
+    return result;
+};
+
+
+
+const add1 = val => val + 1; 
+
+
+module.exports = {
+map,
+add1
+}
---
+++ challenges/arrayMap/array-map.test.js
@@ -0,0 +1,20 @@
+const {
+    map,
+    add1
+}= require('./array-map.js');
+
+
+const array = [1, 2, 3];
+
+describe('validator module', () => {
+describe('basic validation', () => {
+it('1 should be added to each number in the array', () => {
+expect(map(array, add1)).toEqual([2, 3, 4]);
+//         expect(isNumber('hi')).toBeFalsy();
+//         expect(isNumber([])).toBeFalsy();
+//         expect(isNumber({})).toBeFalsy();
+//         expect(isNumber(() => {})).toBeFalsy();
+//         expect(isNumber(true)).toBeFalsy();
+ });
+});
+});
