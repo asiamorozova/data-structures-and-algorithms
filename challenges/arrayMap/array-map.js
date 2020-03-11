a67e29d4398a9e6d921af85fd151c17695c4b1a6
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-11 13:42:41 -0700

    passed

--- challenges/arrayMap/array-map.js
+++ challenges/arrayMap/array-map.js
@@ -1,10 +1,10 @@
 
 const map = (arr, func) => {
-    const result = []
-    for(let i = 0; i < arr.length; i++){
-        result[i] = func(arr[i])
-    }
-    return result;
+  const result = [];
+  for(let i = 0; i < arr.length; i++){
+    result[i] = func(arr[i]);
+  }
+  return result;
 };
 
 
@@ -13,6 +13,6 @@ const add1 = val => val + 1;
 
 
 module.exports = {
-map,
-add1
-}
+  map,
+  add1
+};
