349ab5e9e901308083122b6622a5d7ade3dcda2e
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-20 15:50:47 -0700

    travis

--- arrayShift/array-shift.js
+++ arrayShift/array-shift.js
@@ -1,7 +1,7 @@
 let num = 5; 
 
 const insertShiftArray = () => {
-  arr2 = [2, 4, num, 6, 8];
+  let arr2 = [2, 4, num, 6, 8];
   console.log(arr2);
   return arr2; 
     
--- arrayShift/array-shift.test.js
+++ arrayShift/array-shift.test.js
@@ -2,7 +2,7 @@ const { insertShiftArray } = require('./array-shift.js');
 
 describe('added value', () => {
   it('returns new value in the middle of index', () => {
-    expect(insertShiftArray([2,4,6,8],5)).toEqual([2,4,5,6,8]);
+    expect(insertShiftArray([2, 4, 6, 8], 5)).toEqual([2, 4, 5, 6, 8]);
 
   });
 });
